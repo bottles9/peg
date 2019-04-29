d878e76fdc6159d37c0ef12c2460bab078aeab04
HENRY ANDERSON AOVARE <alg70021@gmail.com>
2019-04-29 16:43:17 +0000

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	redirect_to posts_path
+  end
+
+
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:image, :caption)
+  	
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+	before_action :find_user
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+
+	private
+
+	def find_user
+	  @user = User.find(params[:id])
+		
+	end
+end
